9d06491263e5b3e1412a73a169e2022853113992
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2022-02-15 21:27:50 -0500

    Cleaned script

--- program.py
+++ program.py
@@ -1,23 +1,19 @@
 import neuralnetwork as nn
 import numpy as np
-#import matplotlib.pyplot as plt
 
 with np.load('mnist.npz') as data:
 	training_images = data['training_images']
 	training_labels = data['training_labels']
 
-plt.imshow(training_images[1].reshape(28,28), cmap = 'gray')
-plt.show()
-
-print(training_labels[1])
-print(training_labels.shape)
-
-np.datetime_as_string 
-
-layer_sizes = (3, 5, 10)
-x = np.ones((layer_sizes[0], 1))
+layer_sizes = (784, 5, 10)
 
 net = nn.NeuralNetwork(layer_sizes)
-prediction = net.predict(x)
+net.print_accuracy(training_images, training_labels)
 
-print(prediction)
+
+
+
+#import matplotlib.pyplot as plt
+
+#plt.imshow(training_images[1].reshape(28,28), cmap = 'gray')
+#plt.show()
